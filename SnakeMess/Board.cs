2381afd3e33ce8043debb0e8dabf13d8720758d6
Adrian Rørvik Marti <adrmar15@student.westerdals.no>
2017-10-09 13:52:00 +0200

    Kommenterte litt i board klassen :)

--- SnakeMess/Board.cs
+++ SnakeMess/Board.cs
@@ -5,11 +5,13 @@ using System.Text;
 
 namespace SnakeBeauty
 {
+    // Defines the boundaries of the world
     class Board
     {
         public int Width { get; private set; }
         public int Height { get; private set; }
 
+        //Creates a new board with a width and height fixed to the size of the window
         public Board()
         {
             Width = Console.WindowWidth;
